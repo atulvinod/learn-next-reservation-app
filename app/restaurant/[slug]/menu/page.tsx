4983039df3b21ee5d49be0cbc62ae49874b10d8a
atulvinod <atulvinod1911@gmail.com>
2023-11-04 16:33:57 +0530

    implemented layout file

--- app/restaurant/[slug]/menu/page.tsx
+++ app/restaurant/[slug]/menu/page.tsx
@@ -1,18 +1,15 @@
 import NavBar from "@/app/components/navbar";
 import RestaurantTitle from "../components/title";
 import Menu from "./components/menu";
-import MenuTitle from "./components/menu_title";
 import RestaurantNav from "../components/restaurant_nav";
+import Header from "../components/header";
 
 export default function MenuPage() {
     return (
         <>
-            <MenuTitle />
-            <div className="flex m-auto w-2/3 justify-between items-start 0 -mt-11">
-                <div className="bg-white w-[100%] rounded p-3 shadow">
-                    <RestaurantNav />
-                    <Menu />
-                </div>
+            <div className="bg-white w-[100%] rounded p-3 shadow">
+                <RestaurantNav />
+                <Menu />
             </div>
         </>
     );
--- app/restaurant/[slug]/page.tsx
+++ app/restaurant/[slug]/page.tsx
@@ -10,20 +10,17 @@ import Reservation from "./components/reservation";
 export default function RestaurantDetailsPage() {
     return (
         <>
-            <Header />
-            <div className="flex m-auto w-2/3 justify-between items-start 0 -mt-11">
-                <div className="bg-white w-[70%] rounded p-3 shadow">
-                    <RestaurantNav />
-                    <div className="mt-4 border-b pb-6">
-                        <h1 className="font-bold text-6xl">Milesstone Grill</h1>
-                    </div>
-                    <Rating />
-                    <Description />
-                    <Images />
-                    <Reviews />
+            <div className="bg-white w-[70%] rounded p-3 shadow">
+                <RestaurantNav />
+                <div className="mt-4 border-b pb-6">
+                    <h1 className="font-bold text-6xl">Milesstone Grill</h1>
                 </div>
-                <Reservation />
+                <Rating />
+                <Description />
+                <Images />
+                <Reviews />
             </div>
+            <Reservation />
         </>
     );
 }
